251a982835d9afbda93367e23257d19d4c366f07
Narvik T <narviktahmasian@gmail.com>
2017-04-05 02:01:09 -0700

    Update EchoClient.java

--- EchoClient.java
+++ EchoClient.java
@@ -1,8 +1,11 @@
 
+
 import java.io.InputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.io.OutputStream;
+import java.io.PrintStream;
 
 public final class EchoClient {
 
@@ -12,11 +15,18 @@ public final class EchoClient {
             InputStreamReader isr = new InputStreamReader(is, "UTF-8");
             BufferedReader br = new BufferedReader(isr);
             System.out.println(br.readLine());
+            BufferedReader sbr = new BufferedReader(new InputStreamReader(System.in));
+            OutputStream os = socket.getOutputStream();
+			PrintStream out = new PrintStream(os, true, "UTF-8");
+			System.out.print("Client> ");
+			String text = sbr.readLine();		
+			while (!text.equals("exit")) {
+				out.println(text);			
+				System.out.println("Server> " + br.readLine());
+				System.out.print("Client> ");
+				text = sbr.readLine();
+			}
+			out.print("exit");
         }
     }
 }
-
-
-
-
-
